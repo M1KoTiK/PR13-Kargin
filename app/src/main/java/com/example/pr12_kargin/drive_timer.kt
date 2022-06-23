e50a1b2c70fe625a8a54e8338c14e43992f15810
KoTiK <temhbz@mail.ru>
2022-06-24 02:07:58 +0700

    тост вместо последнего экрана, наверное норм

--- app/src/main/java/com/example/pr12_kargin/drive_timer.kt
+++ app/src/main/java/com/example/pr12_kargin/drive_timer.kt
@@ -1,11 +1,24 @@
 package com.example.pr12_kargin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class drive_timer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drive_timer)
+        val btnStop2 = findViewById<Button>(R.id.btn_stop2)
+        btnStop2.setOnClickListener {
+            val toast = Toast.makeText(this, "Thanks!", Toast.LENGTH_LONG)
+            toast.setGravity(Gravity.TOP, 0, 300) // import android.view.Gravity;
+
+            toast.show()
+        }
+
+
+
     }
 }
