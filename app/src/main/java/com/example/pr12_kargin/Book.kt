3f379075767116462f7c4793819335bb9566d5d7
KoTiK <temhbz@mail.ru>
2022-06-23 17:02:25 +0700

    добавлены все экраны

--- app/src/main/java/com/example/pr12_kargin/Book.kt
+++ app/src/main/java/com/example/pr12_kargin/Book.kt
@@ -3,14 +3,18 @@ package com.example.pr12_kargin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 
 class Book : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book)
-
-
+        val btnBook = findViewById<Button>(R.id.btn_book)
+        btnBook.setOnClickListener{
+            val openWait = Intent(this, wait_timer::class.java)
+            startActivity(openWait)
+        }
 
 
 
--- app/src/main/java/com/example/pr12_kargin/wait_timer.kt
+++ app/src/main/java/com/example/pr12_kargin/wait_timer.kt
@@ -1,11 +1,25 @@
 package com.example.pr12_kargin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class wait_timer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wait_timer)
+
+        val btnStop = findViewById<Button>(R.id.btn_stop)
+        btnStop.setOnClickListener{
+            val openMenus = Intent(this, drive_timer::class.java)
+            startActivity(openMenus)
+        }
+        val btnCancel = findViewById<Button>(R.id.btn_cancel)
+        btnCancel.setOnClickListener{
+            val openMenus = Intent(this, Start_Screen::class.java)
+            startActivity(openMenus)
+        }
+
     }
 }
